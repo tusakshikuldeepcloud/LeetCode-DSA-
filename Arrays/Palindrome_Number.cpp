45bc58d1235a376dc485de9f172fec18f0629d78
Tusakshi kuldeep <tusakshi12kuldeep@gmail.com>
2026-08-03 01:35:27 +0530

    Palindrome_Number.cpp

---
+++ Arrays/Palindrome_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {string s = to_string(x);
+
+        string rev = s;
+
+        reverse(rev.begin(), rev.end());
+
+        return s == rev;
+        
+    }
+};
