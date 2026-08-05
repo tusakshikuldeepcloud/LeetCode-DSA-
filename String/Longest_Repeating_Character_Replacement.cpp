adab614487d7dd335685c7647dab630e2589c3a5
Tusakshi kuldeep <tusakshi12kuldeep@gmail.com>
2026-08-05 14:04:57 +0530

    Longest Repeating Character Replacement solution

---
+++ String/Longest_Repeating_Character_Replacement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+
+        vector<int> freq(26, 0);
+
+        int left = 0;
+        int maxFreq = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            freq[s[right] - 'A']++;
+
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) {
+
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
